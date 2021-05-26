f23401a27be0d4beec3fac8603b02e4eb1874af7
rajahd2 <rajahd2@wwu.edu>
2021-05-26 00:01:57 -0700

    Removed punctuations from vocabs, fixed formatting

--- consolidateText.py
+++ consolidateText.py
@@ -1,9 +1,9 @@
 import glob
+import re
+import textwrap
 
 
-def combine(path,name):
-
-
+def combine(path, name):
     read_files = glob.glob(path + "*.txt")
 
     with open(name, "wb") as outfile:
@@ -12,7 +12,22 @@ def combine(path,name):
                 outfile.write(infile.read())
 
 
+def removePunc(path):
+    file1 = open(path, 'r')
+    Lines = file1.readlines()
+    my_file = open("goodHorror.txt", "w")
+
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        res = re.sub(r'[^\w\s]', '', line)
+        res = '\n'.join(textwrap.wrap(res, 100, break_long_words=False))
+        my_file.write(res)
+
+
 
 if __name__ == "__main__":
-    path = "/Users/dylonrajah/Desktop/404Final/GoodSciFi/"
-    combine(path,"GoodSciFi.txt")
+    path = "/Users/dylonrajah/Desktop/404Final/CombinedVocabs/GH.txt"
+    # combine(path,"GoodSciFi.txt")
+    removePunc(path)
