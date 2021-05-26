024523940187b10eb29594ecbfc9d9bdec048be1
jacksor9 <jacksor9@wwu.edu>
2021-05-26 13:59:15 -0700

    initial setup and renaming of some files

---
+++ main.py
@@ -0,0 +1,6 @@
+def load_reviews():
+
+
+
+if __name__ == '__main__':
+    badAction, badComedy, badHorror, badRomance, badSciFi, goodAction, goodComedy, goodHorror, goodRomance, goodSciFi = load_reviews()
