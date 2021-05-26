ef04cc6de4b0c7b8c103260bd35b3cb2848a69c7
jacksor9 <jacksor9@wwu.edu>
2021-05-26 15:14:36 -0700

    quick loading in of some reviews

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 def load_reviews():
+    badActionList = []
+    badActionString = ""
+    with open('CombinedVocabs/badAction.txt', 'r') as badAction:
+        for line in badAction:
+            badActionString += remove_combined_words(line) + " "
+        badActionList.append(badActionString)
+    print(badActionList)
 
+def remove_combined_words(sentence):
+    output = ""
+    splitSentence = sentence.split(' ')
+    for i in splitSentence:
+        if i[1:] != i[1:].lower() and i[1:] != i[1:].upper():
+            for j in range(1, len(i[1:])):
+                if i[j] == i[j].upper() and i[j-1] == i[j-1].lower():
+                    output += i[:j].lower() + " "
+                    output += i[j:].lower() + " "
+                    break
+        else:
+            output += i.lower() + " "
+    return output
 
 
 if __name__ == '__main__':
-    badAction, badComedy, badHorror, badRomance, badSciFi, goodAction, goodComedy, goodHorror, goodRomance, goodSciFi = load_reviews()
+
+    sentence = "one two three fourFive six"
+    print(remove_combined_words(sentence))
+
+    load_reviews()
+    #badAction, badComedy, badHorror, badRomance, badSciFi, goodAction, goodComedy, goodHorror, goodRomance, goodSciFi = load_reviews()
