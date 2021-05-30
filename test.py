3dff2f24fe2f9377946c55c95c8c99bda387fef5
jacksor9 <jacksor9@wwu.edu>
2021-05-30 13:12:09 -0700

    working on opinion word mining

--- test.py
+++ test.py
@@ -1,38 +1,48 @@
 import spacy
 from spacy import displacy
 
-#DEPENDENCY
-text = "When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
+#opinion lexicon
+#https://www.kaggle.com/nltkdata/opinion-lexicon
+
+def get_opinion_dicts():
+    with open('positive-words.txt') as f:
+        positive = f.read().splitlines()[30:]
+    with open('negative-words.txt') as f:
+        negative = f.read().splitlines()[31:]
+    return positive, negative
+
+positive_words, negative_words = get_opinion_dicts()
 
 #used for dependency visualization
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(text)
 #displacy.serve(doc, style="dep")
 #serves on http://127.0.0.1:5000/
 
 import re
 
-#TOKENIZATION
+#TOKENIZATION and DEPENDENCY PARSING on reviews
 with open('GenreReviews/BadActionReviews/badDunkirk.txt', encoding='utf8') as f:
     text = f.read()
     text = text.lower()
     text = re.sub(r'\?+', '.', text)
     text = re.sub(r'\!+', '.', text)
     text = re.sub(r'\.+', '.', text)
-    print(text)
+    #print(text)
     print('---------------')
     doc = nlp(text)
-    for token in doc:
-        if token.
     sentences = list(doc.sents)
-    for i in sentences:
-        print('|' , i , '|')
+    #displacy.serve(sentences, style="dep")
+    for sentence in sentences:
+        print('|' , sentence , '|')
+        for token in sentence:
+            #print(token.text, token.tag_, token.head.text, token.dep_)
+            if token.text in positive_words:
+                print(token, ': pos')
+            if token.text in negative_words:
+                print(token, ': neg')
 
-#DEPENDENCY PARSING
+#DEPENDENCY PARSING EXAMPLE
 text = 'this Film had great acting.'
 doc = nlp(text)
-for token in doc:
-    if token.is_punct:
-        print(token, ' is a punct!')
 for token in doc:
     print(token.text, token.tag_, token.head.text, token.dep_)
